c88b95ecfd0d9ab6b11cd7043422d1efa6c5a8f6
AlexeyKozlovsky <kozaa.19@uni-dubna.ru>
2021-10-23 06:56:20 +0300

    added full roadgraph page

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from routes.car import car_router
 from routes.route import route_router
@@ -7,14 +10,16 @@ from routes.graph import graph_router
 
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
 app.include_router(car_router)
 app.include_router(route_router)
 app.include_router(prop_router)
 app.include_router(graph_router)
 
 
-@app.get('/')
-async def main_page():
-    return {
-        'message': 'tilt'
-    }
+@app.get('/road_graph', include_in_schema=False, response_class=HTMLResponse)
+async def road_graph_page(request: Request):
+    return templates.TemplateResponse('graph_map.html', {'request': request})
