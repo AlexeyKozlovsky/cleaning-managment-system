842fae93c1c126b2715b230fcafb561e6d74e1e5
AlexeyKozlovsky <kozaa.19@uni-dubna.ru>
2021-10-24 08:17:30 +0300

    added cors

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
 
 from routes.car import car_router
 from routes.renderer import renderer
@@ -14,6 +15,16 @@ app = FastAPI()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
+
+origins = ['80.87.111.51:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'POST'],
+    allow_headers=['*']
+)
 
 app.include_router(car_router)
 app.include_router(route_router)
